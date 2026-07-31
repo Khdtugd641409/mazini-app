98b49837defc7c3a4ecf31db40d8361be3e4094e
Khdtugd641409 <tasis2030@gmail.com>
2026-07-31 15:58:16 +0300

    Create CustomerApplicationPage.jsx

---
+++ src/pages/CustomerApplicationPage.jsx
@@ -0,0 +1,10 @@
+function CustomerApplicationPage() {
+  return (
+    <main>
+      <h1>تقديم طلب البناء الذاتي</h1>
+      <p>سيُنشأ نموذج العميل هنا.</p>
+    </main>
+  );
+}
+
+export default CustomerApplicationPage;
