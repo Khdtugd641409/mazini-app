a8132c18b8bf15774456255993c692f116171421
Khdtugd641409 <tasis2030@gmail.com>
2026-07-31 15:59:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import HomePage from "./pages/HomePage.jsx";
+import CustomerApplicationPage from "./pages/CustomerApplicationPage.jsx";
 
 function App() {
-  return <HomePage />;
+  const [currentPage, setCurrentPage] = useState("home");
+
+  if (currentPage === "customer-application") {
+    return (
+      <CustomerApplicationPage
+        onBack={() => setCurrentPage("home")}
+      />
+    );
+  }
+
+  return (
+    <HomePage
+      onOpenCustomerApplication={() =>
+        setCurrentPage("customer-application")
+      }
+    />
+  );
 }
 
 export default App;
