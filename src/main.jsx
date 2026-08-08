47f2013426e0e9dbc02c2eb31da1edf2f4e4b3ed
Khdtugd641409 <tasis2030@gmail.com>
2026-08-08 18:52:10 +0300

    Route supervisor application page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import SupervisorApplicationPage from "./pages/SupervisorApplicationPage.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+const normalizedPath = window.location.pathname.replace(/\/+$/, "") || "/";
+
+const rootContent =
+  normalizedPath === "/supervisor/application" ? (
+    <SupervisorApplicationPage
+      onBack={() => {
+        window.location.href = "/";
+      }}
+      onOpenSupervisor={() => {
+        window.location.href = "/supervisor";
+      }}
+    />
+  ) : (
     <App />
-  </React.StrictMode>
+  );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>{rootContent}</React.StrictMode>
 );
