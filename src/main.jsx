65498b32bc2d94a7b085c466f5a7f7ee1cd57440
Khdtugd641409 <tasis2030@gmail.com>
2026-08-08 18:53:49 +0300

    Route admin supervisor application review

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import SupervisorApplicationPage from "./pages/SupervisorApplicationPage.jsx";
+import AdminSupervisorApplicationsPage from "./pages/admin/AdminSupervisorApplicationsPage.jsx";
 import "./index.css";
 
 const normalizedPath = window.location.pathname.replace(/\/+$/, "") || "/";
 
-const rootContent =
-  normalizedPath === "/supervisor/application" ? (
+let rootContent;
+
+if (normalizedPath === "/supervisor/application") {
+  rootContent = (
     <SupervisorApplicationPage
       onBack={() => {
         window.location.href = "/";
@@ -16,9 +19,12 @@ const rootContent =
         window.location.href = "/supervisor";
       }}
     />
-  ) : (
-    <App />
   );
+} else if (normalizedPath === "/admin/supervisor-applications") {
+  rootContent = <AdminSupervisorApplicationsPage />;
+} else {
+  rootContent = <App />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>{rootContent}</React.StrictMode>
